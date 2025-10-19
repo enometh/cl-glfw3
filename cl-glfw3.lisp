5e13afbf4837c88ba2ba0418c90f528068a3a495
Madhu <enometh@net.meer>
2025-10-19 06:03:55 +0530

    implement wait-events-timeout
    * glfw3-bindings.liep: (wait-event-timout): defined and exported
    * cl-glfw3.lisp: (%glfw:wait-events-timeout): import-exported.

--- cl-glfw3.lisp
+++ cl-glfw3.lisp
@@ -351,7 +351,9 @@ SHARED: The window whose context to share resources with."
   (%glfw:set-framebuffer-size-callback window (cffi:get-callback callback-name)))
 
 ;;;; ## Events and input
-(import-export %glfw:poll-events %glfw:wait-events %glfw:post-empty-event)
+(import-export %glfw:poll-events %glfw:wait-events
+	       %glfw:wait-events-timeout
+	       %glfw:post-empty-event)
 
 (defun get-input-mode (mode &optional (window *window*))
   "Mode is one of :CURSOR :STICKY-KEYS or :STICKY-MOUSE-BUTTONS."
